623fc9defe9d02f5dc3fba27d88c18f6e38aea05
encodi <encodari@gmail.com>
2022-11-08 12:10:20 -0600

    fix: Removes callback hell

--- spider.js
+++ spider.js
@@ -4,32 +4,42 @@ import superagent from 'superagent';
 import mkdirp from 'mkdirp';
 import { urlToFilename } from "./util.js";
 
+function saveFile(fileName, contents, cb) {
+  mkdirp(path.dirname(fileName), err => {
+    if (err) {
+      return cb(err);
+    }
+    fs.writeFile(fileName, contents, cb);
+  });
+}
+
+function download (url, fileName, cb) {
+  console.log(`Downloading ${url} into ${fileName}`);
+  superagent.get(url).end((err, res) => {
+    if (err) {
+      return cb(err);
+    }
+    saveFile(fileName, res.text, err => {
+      if (err) {
+        return cb(err);
+      }
+      console.log(`Downloaded and saved: ${url}`);
+      cb(null, res.text);
+    });
+  });
+}
+
 export function spider(url, cb) {
   const fileName = urlToFilename(url);
   fs.access(fileName, err => {
-    if (err && err.code === 'ENOENT') {
-      console.log(`Downloading ${url} into ${fileName}`);
-      superagent.get(url).end((err, res) => {
-        if (err) {
-          cb(err);
-        } else {
-          mkdirp(path.dirname(fileName), err => {
-            if (err) {
-              cb(err);
-            } else {
-              fs.writeFile(fileName, res.text, err => {
-                if (err) {
-                  cb(err);
-                } else {
-                  cb(null, fileName, true);
-                }
-              });
-            }
-          });
-        }
-      });
-    } else {
-      cb(null, fileName, false);
+    if (!err || err.code !== 'ENOENT') {
+      return cb(null, fileName, false);
     }
+    download(url, fileName, err => {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, fileName, true);
+    });
   });
 }
